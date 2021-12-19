4df669c1c7787c2d7c0b7de64e673babec1ca28f
Kelvin <kevthedev007@gmail..com>
2021-12-18 17:42:59 -0800

    store

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -8,16 +8,16 @@ const addItem = async (req, res) => {
     const result = await cloudinary.uploader.upload(image, {
       upload_preset: "store_images",
     });
-      //insert in store
-      const addToStore = await Store.create({
-        userId: req.user.id,
-        name,
-        size,
-        color,
-        price,
-        image: result.secure_url,
-        public_id: result.public_id
-      }) 
+    //insert in store
+    const addToStore = await Store.create({
+      userId: req.user.id,
+      name,
+      size,
+      color,
+      price,
+      image: result.secure_url,
+      public_id: result.public_id
+    })
 
     return res.status(200).json("store item added successfully")
   } catch (error) {
@@ -32,20 +32,20 @@ const getStore = async (req, res) => {
     if (!stores) return res.status(200).send("There are no available items now");
 
     //check store_items owned by user
-    let availableItems = [];
+    //   let availableItems = [];
 
-    for (let i in stores) {
-      if (stores[i].userId == req.user.id) {
-          stores[i].isUser = true
-          availableItems.push(stores[i])
-      } else {
-          stores[i].isUser = false
-          availableItems.push(stores[i])
-      }
-  }
+    //   for (let i in stores) {
+    //     if (stores[i].userId == req.user.id) {
+    //         stores[i].isUser = true
+    //         availableItems.push(stores[i])
+    //     } else {
+    //         stores[i].isUser = false
+    //         availableItems.push(stores[i])
+    //     }
+    // }
 
-  return res.status(200).json(availableItems)
-    
+    return res.status(200).json(stores)
+
   } catch (error) {
     res.status(500).json(error.message)
   }
@@ -57,14 +57,14 @@ const getStoreDetails = async (req, res) => {
   try {
     const user = await User.findOne({
       where: {
-          id: req.user.id,
+        id: req.user.id,
       },
       include: ['store', 'model']
-  })
-  const images = await Store_Images.findAll({ where: { userId: req.user.id, storeId: id } })
-  
+    })
+    const images = await Store_Images.findAll({ where: { userId: req.user.id, storeId: id } })
+
     if (user.id == req.user.id) return res.status(200).json({ user, images, isUser: true })
-        else return res.status(200).json({ user, images, isUser: false })
+    else return res.status(200).json({ user, images, isUser: false })
 
   } catch (error) {
     res.status(500).json(error.message)
@@ -77,15 +77,15 @@ const addImages = async (req, res) => {
   try {
     const result = await cloudinary.uploader.upload(image, {
       upload_preset: "store_images",
-  });
+    });
 
-  const uploadImage = await Store_Images.create({
-    userId: req.user.id,
-    storeId: id,
-    image: result.secure_url,
-    public_id: result.public_id
-  })
-    
+    const uploadImage = await Store_Images.create({
+      userId: req.user.id,
+      storeId: id,
+      image: result.secure_url,
+      public_id: result.public_id
+    })
+
     return res.status(200).json("image uploaded successfully")
   } catch (error) {
     res.status(400).json(error.message)
@@ -96,17 +96,17 @@ const deleteStoreItem = async (req, res) => {
   const { id } = req.params;
 
   try {
-        //delete image in cloud
+    //delete image in cloud
     const store = await Store.findOne({ where: { id } })
-  
-        await cloudinary.uploader.destroy(store.public_id)
-        const images = await Store_Images.findAll({ where: {storeId: id}})
-        for (let i in images) {
-          await cloudinary.uploader.destroy(images[i].public_id)
-        }
 
-        await store.destroy()
-        return res.status(200).send('store item deleted successfully')
+    await cloudinary.uploader.destroy(store.public_id)
+    const images = await Store_Images.findAll({ where: { storeId: id } })
+    for (let i in images) {
+      await cloudinary.uploader.destroy(images[i].public_id)
+    }
+
+    await store.destroy()
+    return res.status(200).send('store item deleted successfully')
   } catch (error) {
     res.status(500).json(error.message)
   }
