831812d31f47bd269574dfe33655db8a89698606
Kelvin <kevthedev007@gmail..com>
2021-12-20 10:30:28 -0800

    cron

--- job.js
+++ job.js
@@ -1,17 +1,15 @@
 const cron = require('node-cron');
 const { Subscription } = require('./models')
 
-cron.schedule("* * * * *", async () => {
+cron.schedule("*/15 * * * *", async () => {
     // TODO: subscritpion expiring
     console.log('job running')
     const sub = await Subscription.findAll();
 
     for await (let i of sub) {
-        console.log(i)
         let date1 = new Date(i.expires_in);
         let date2 = new Date()
-        console.log(date1)
-        console.log(date2)
+
         if (date1.getTime() < date2.getTime()) {
             i.destroy()
         }
